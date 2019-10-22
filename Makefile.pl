fc6bc2c3a2039407ab1247c6aefccb20d91994b7
Andrey Vorobyev <vorobivka@mail.ru>
2019-10-22 09:10:42 +0000

    Debug slack message

--- Makefile.pl
+++ Makefile.pl
@@ -2,7 +2,7 @@ use ExtUtils::MakeMaker;
 
 WriteMakefile(
     NAME            => 'Net::Sms::TextMagicClient',
-    VERSION => 2.0.673,
+    VERSION => 2.0.674,
     PREREQ_PM  => {
         "URI::Query" => 0,
         "Log::Any" => 0,
